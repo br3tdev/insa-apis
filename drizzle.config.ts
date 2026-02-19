64b4ffafbfb03837f169b9f53be085d3cc0eb032
Bret Mda <brtdev2@gmail.com>
2026-02-19 08:36:26 +0300

    Configure postgres db

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from "drizzle-kit";
 
-import env from "@/env";
+import env from "./src/env";
 
 export default defineConfig({
   schema: "./src/db/schema.ts",
   out: "./src/db/migrations",
-  dialect: "turso",
-  casing: "snake_case",
+  dialect: "postgresql",
   dbCredentials: {
     url: env.DATABASE_URL,
-    authToken: env.DATABASE_AUTH_TOKEN,
   },
 });
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,16 +1,16 @@
-import { drizzle } from "drizzle-orm/libsql";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
 
 import env from "@/env";
 
 import * as schema from "./schema";
 
-const db = drizzle({
-  connection: {
-    url: env.DATABASE_URL,
-    authToken: env.DATABASE_AUTH_TOKEN,
-  },
-  casing: "snake_case",
-  schema,
+const client = postgres(env.DATABASE_URL, {
+  idle_timeout: 20,
+  max_lifetime: 60 * 30,
 });
 
+const db = drizzle({ client, schema });
+
 export default db;
+export { schema };
